a9801e43d01b826bb01dbaa4d9cf87b84b3ee989
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-07-31 16:48:11 -0500

    commitR3

--- ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/GestorDB.java
+++ ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/GestorDB.java
@@ -1,8 +1,13 @@
 package com.aprendiz.ragp.concentresep1.models;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorDB extends SQLiteOpenHelper{
     public GestorDB(Context context) {
@@ -20,9 +25,39 @@ public class GestorDB extends SQLiteOpenHelper{
     }
 
 
-    public void inputData(){
+    public void inputData(Score score){
         SQLiteDatabase db = getWritableDatabase();
-        
+        ContentValues values = new ContentValues();
+        values.put("JUGADOR",score.getJugador());
+        values.put("PUNTUACION",score.getPuntuacion());
+        values.put("DIFICULTAD",score.getDifcultad());
+        values.put("MODO",score.getModo());
+        db.insert("SCORE",null,values);
+        db.close();
+
+    }
+
+    public List<Score> scoreList(String dificultad, String modo){
+        List<Score> results = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM SCORE WHERE DIFICULTAD = '"+dificultad+"' AND MODO='"+modo+"'; ORDER BY PUNTUACION DESC",null);
+        if (cursor.moveToFirst()){
+            do {
+                Score score = new Score();
+                score.setJugador(cursor.getString(0));
+                score.setPuntuacion(cursor.getString(1));
+                score.setDifcultad(cursor.getString(2));
+                score.setModo(cursor.getString(3));
+
+                results.add(score);
+
+            }while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return results;
     }
 
 
