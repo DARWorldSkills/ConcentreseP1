e3be5894043c2d110c87ab39342662743d5f0d88
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-07-31 16:37:53 -0500

    commitR"

--- ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/GestorDB.java
+++ ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/GestorDB.java
@@ -1,4 +1,29 @@
 package com.aprendiz.ragp.concentresep1.models;
 
-public class GestorDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GestorDB extends SQLiteOpenHelper{
+    public GestorDB(Context context) {
+        super(context, "concentrese.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE SCORE (JUGADOR TEXT, PUNTUACION TEXT, DIFICULTAD TEXT, MODO TEXT)");;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+    public void inputData(){
+        SQLiteDatabase db = getWritableDatabase();
+        
+    }
+
+
 }
--- ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/Score.java
+++ ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/models/Score.java
@@ -1,4 +1,43 @@
 package com.aprendiz.ragp.concentresep1.models;
 
 public class Score {
+    private String jugador;
+    private String puntuacion;
+    private String difcultad;
+    private String modo;
+
+    public Score() {
+    }
+
+    public String getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(String jugador) {
+        this.jugador = jugador;
+    }
+
+    public String getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(String puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getDifcultad() {
+        return difcultad;
+    }
+
+    public void setDifcultad(String difcultad) {
+        this.difcultad = difcultad;
+    }
+
+    public String getModo() {
+        return modo;
+    }
+
+    public void setModo(String modo) {
+        this.modo = modo;
+    }
 }
