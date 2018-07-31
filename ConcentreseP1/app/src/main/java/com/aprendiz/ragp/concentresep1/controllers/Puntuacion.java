58d433b8dc7b6eeea9a2a9ebaf91f955cca42b37
DavidPyn27 <popayan159@gmail.com>
2018-07-31 16:17:23 -0500

    commit12as

--- ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/controllers/Puntuacion.java
+++ ConcentreseP1/app/src/main/java/com/aprendiz/ragp/concentresep1/controllers/Puntuacion.java
@@ -2,14 +2,18 @@ package com.aprendiz.ragp.concentresep1.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.aprendiz.ragp.concentresep1.R;
 
 public class Puntuacion extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntuacion);
+
+
     }
 }
